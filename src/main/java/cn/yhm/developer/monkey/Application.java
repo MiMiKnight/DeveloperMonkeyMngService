a2514d0d3e002db0de2b8b4f9830b4eea4c3d957
Jack <victor2015yhm@gmail.com>
2022-10-11 00:13:46 +0800

    【关联单号】US202210092400 【修改类型】Feature 【提交描述】补充README文件描述，优化代码

--- src/main/java/cn/yhm/developer/monkey/Application.java
+++ src/main/java/cn/yhm/developer/monkey/Application.java
@@ -3,22 +3,23 @@ package cn.yhm.developer.monkey;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 /**
  * 启动程序
  *
  * @author victor2015yhm@gmail.com
- * @since 2022-09-14 22:00:56
+ * @since 2022-10-11 00:10:10
  */
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"cn.yhm.developer.monkey", "cn.yhm.developer.ecology"})
 public class Application {
 
+    private static final String APP_NAME = "DeveloperMonkeyMngService";
+
     public static void main(String[] args) {
         // 启动项目
         SpringApplication.run(Application.class, args);
-        log.info("The project started successfully.");
+        log.info("The project [" + APP_NAME + "] started successfully.");
     }
 }
